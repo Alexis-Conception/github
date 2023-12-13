e7219c01d96ab540ea5bfd8a40ac2cdc58fcc96c
AlexisHenry <alexis.henry150357@gmail.com>
2023-12-13 21:33:39 +0100

    fix: exception error

--- src/Exceptions/GithubTokenNotSetException.php
+++ src/Exceptions/GithubTokenNotSetException.php
@@ -6,6 +6,6 @@ class GithubTokenNotSetException extends \Exception
 {
     public function __construct()
     {
-        parent::__construct('You must set a Github token to create a repository.');
+        parent::__construct('You must set a Github token to communicate with the GitHub API.');
     }
 }
